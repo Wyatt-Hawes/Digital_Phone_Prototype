7390d825801b33523641e1e2248712828b8663da
Adam Smith <adam@adamsmith.as>
2023-10-10 16:26:50 -0700

    Added example png asset

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -1,16 +1,23 @@
 import * as Phaser from "phaser";
 
+import starfieldUrl from "/assets/starfield.png";
+
 export default class Play extends Phaser.Scene {
   fire?: Phaser.Input.Keyboard.Key;
   left?: Phaser.Input.Keyboard.Key;
   right?: Phaser.Input.Keyboard.Key;
 
+  starfield?: Phaser.GameObjects.TileSprite;
   spinner?: Phaser.GameObjects.Shape;
 
   rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
 
   constructor() {
     super("play");
+  }
+
+  preload() {
+    this.load.image("starfield", starfieldUrl);
   }
 
   #addKey(
@@ -24,10 +31,22 @@ export default class Play extends Phaser.Scene {
     this.left = this.#addKey("LEFT");
     this.right = this.#addKey("RIGHT");
 
+    this.starfield = this.add
+      .tileSprite(
+        0,
+        0,
+        this.game.config.width as number,
+        this.game.config.height as number,
+        "starfield",
+      )
+      .setOrigin(0, 0);
+
     this.spinner = this.add.rectangle(100, 100, 50, 50, 0xff0000);
   }
 
   update(_timeMs: number, delta: number) {
+    this.starfield!.tilePositionX -= 4;
+
     if (this.left!.isDown) {
       this.spinner!.rotation -= delta * this.rotationSpeed;
     }
